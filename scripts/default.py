9f54146c0eb4b70b56e22a5e2b397132a9e4522a
Myungcheol Lee <mclee@etri.re.kr>
2020-11-10 16:50:26 +0900

    Modified for integration with VisServer

---
+++ scripts/default.py
@@ -0,0 +1,50 @@
+import argparse
+import libsalt
+import math
+import os
+
+cwd_path = os.getcwd()
+this_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+salt_core_root_dir = os.path.join(this_file_dir_path, "..")
+
+default_conf_path = os.path.join(salt_core_root_dir, "conf", "salt.conf.json")
+default_scenario_path = os.path.join(salt_core_root_dir, "data", "scenario.default.json")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run Dynamic Simulation")
+    parser.add_argument('-c', '--conf', nargs='?', default=default_conf_path)
+    parser.add_argument('-s', '--scenario', nargs='?', default=default_scenario_path)
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    confPath = os.path.join(cwd_path, args.conf)
+    scenarioPath = os.path.join(cwd_path, args.scenario)
+    print('args', args)
+    print('conf path', confPath)
+    print('scenario path', scenarioPath)
+
+    #confJson = getJsonObj(confPath)
+    #print('conf json', confJson)
+    #scenarioJson = getJsonObj(scenarioPath)
+    #print('scenario json', scenarioJson)
+
+    # ======================================
+
+    libsalt.start(scenarioPath)
+    #libsalt.setCurrentStep(25200)
+    step = libsalt.getCurrentStep()
+    while step <= 87000:
+        libsalt.simulationStep()
+        step = libsalt.getCurrentStep()
+
+    libsalt.close()
+    print("Python: Simulation End!!!")
+
+    # ======================================
+
+    exit(0)
+
+main()
+
---
+++ scripts/default.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=/home/ubuntu/uniq-sim/tools/libsalt python default.py -s /home/ubuntu/uniq-sim/data/SALT_202009_00940/SALT_202009_00940.json
